831baf3e171cb1bbc33d8ca60392bb742638022a
Jaime Pokhoy <jaimepokhoy@gmail.com>
2018-08-28 22:22:41 -0400

    Include winning logic

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,23 +6,52 @@ export default class Game extends Component {
         super();
 
         this.state = {
-            grid: ['','','X','','','','O','',''],
-            xIsNext: true
+            grid: ['','','','','','','','',''],
+            xIsNext: true,
+            winner: false
         }
 
         this.handleClick = this.handleClick.bind(this);
     }
 
     handleClick(index) {
-        const { xIsNext, grid } = this.state;
+        const { xIsNext, grid, winner } = this.state;
+
+        if (winner)
+            return;
+
         const squares = grid.slice();
 
         squares[index] = xIsNext ? 'X' : 'O';
 
+        const gameWinner = this.checkWinner(squares);
+
         this.setState({
             grid: squares,
-            xIsNext: !xIsNext
+            xIsNext: !xIsNext,
+            winner: gameWinner
         });
+    }
+
+    checkWinner(grid) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (grid[a] && grid[a] === grid[b] && grid[a] === grid[c])
+                return grid[a];
+        }
+
+        return null;
     }
 
     render() {
